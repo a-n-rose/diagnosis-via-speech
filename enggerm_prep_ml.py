f343734b7306512adcac7d58a2879e9eb3f965f4
a-n-rose <37121771+a-n-rose@users.noreply.github.com>
2018-03-08 21:31:54 +0100

    Update enggerm_prep_ml.py
    improved some code

--- enggerm_prep_ml.py
+++ enggerm_prep_ml.py
@@ -2,8 +2,8 @@
 takes in .csv with speaker, 40 columns for mfcc, and label (English, German) and turns it to machine learning friendly format
 '''
 import pandas as pd
-file = pd.read_csv("sp_df_Eng_Germ2.csv")
-ml_file = file.ix[:,"0":"39"]
+df = pd.read_csv("sp_df_Eng_Germ2.csv")
+ml_df = df.ix[:,"0":"39"]
 #one-hot-encode English and German
-ml_file["language"] = file["language"]
-ml_file.to_csv("engerm_mfcc_ml.csv")
+ml_df["language"] = df["label"]=="English"
+ml_df.to_csv("engerm_mfcc_ml.csv")
