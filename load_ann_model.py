649552c6ab224701d0541223585eb5ad8c231cf2
a-n-rose <37121771+a-n-rose@users.noreply.github.com>
2018-03-08 18:16:45 +0100

    Update load_ann_model.py
    Edited script to remove unnecessary imports, add description, updated names for files

--- load_ann_model.py
+++ load_ann_model.py
@@ -1,24 +1,24 @@
-# MLP for Pima Indians Dataset Serialize to JSON and HDF5
-from keras.models import Sequential
-from keras.layers import Dense
+'''
+uploads previously saved ANN model, specific for the English-German MFCC ANN model
+runs the model with "new" data and prints accuracy
+'''
 from keras.models import model_from_json
-import numpy
-import os
 import pandas as pd
 import numpy as np
 
 
+
 # load json and create model
-json_file = open('model.json', 'r')
+json_file = open('engerm_model_ann.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-loaded_model.load_weights("engerm_50epochs_weights_ann.h5")
+loaded_model.load_weights("engerm_weights_ann.h5")
 print("Loaded model from disk")
 
 
-
+#import "test" data (same data as from before... this is just to show it works)
 sp_df = pd.read_csv("engerm_mfcc_ml.csv",index_col = "Unnamed: 0")
 X = np.array(sp_df.ix[:,:-1])
 y = np.array(sp_df.ix[:,-1])
