e9b579735a39b02a75a2b32dc6c6ce728347026b
a-n-rose <37121771+a-n-rose@users.noreply.github.com>
2018-04-18 16:32:54 +0200

    Update extract_13MFCC_sqlite3.py
    Added progress display via tqdm

--- extract_13MFCC_sqlite3.py
+++ extract_13MFCC_sqlite3.py
@@ -1,14 +1,12 @@
 '''
 This script is suited to exctract features from .wav files that are in one or more folders within the cwd.  
 
-There are 13 MFCCs collected, which have 25ms window frames with 10ms shifts.
+There are 13 MFCCs, with 25ms window frames and 10ms shifts that are extracted.
 
-The MFCCs will be saved to a database in the cwd via SQLite3 
+The MFCCs will be saved to a database in the cwd via SQLite3. The progress is printed by tqdm.
 
-note: to distinguish the differences of the files between the folders, the "label" (i.e. the input provided by the user) 
-is combined with the file's folder name and saved in the database under the "label" column.
-
-The number of MFCCs reflect the number of columns in the database, plus 2 for the filename and the label.
+note: to distinguish the differences of the files between the folders, the "label" 
+(i.e. the input provided by the user) is combined with the file's folder name and saved in the database.
 '''
 
 
@@ -20,22 +18,19 @@ import librosa
 import glob
 import sqlite3
 
+from tqdm import tqdm, tqdm_pandas
+
 
 def parser(file,num_mfcc):
     y, sr = librosa.load(file, res_type= 'kaiser_fast')
     mfccs = librosa.feature.mfcc(y, sr, n_mfcc=num_mfcc,hop_length=int(0.010*sr),n_fft=int(0.025*sr))
     return mfccs
 
-def get_save_mfcc(tgz_file,label,dirname,num_mcff):
+def get_save_mfcc(tgz_file,label,dirname,num_mcff,tot_wav):
     label = label+"_"+dirname
     try:
         filename = os.path.splitext(tgz_file)[0]
-        print("Filename documented")
-        print(filename)
-        print("Extracting MFCC / features")
         feature = parser(filename+".wav",num_mcff)
-        print("MFCCs have been extracted")
-        print("Creating dataframe for the speaker's MFCC data")
         columns = list((range(0,num_mfcc)))
         column_str = []
         for i in columns:
@@ -47,7 +42,6 @@ def get_save_mfcc(tgz_file,label,dirname,num_mcff):
         curr_db["label"] = label
         x = curr_db.as_matrix()
         num_cols = 2+num_mfcc
-        print(num_cols)
         col_var = ""
         for i in range(num_cols):
             if i < num_cols-1:
@@ -57,7 +51,6 @@ def get_save_mfcc(tgz_file,label,dirname,num_mcff):
         print()
         c.executemany(' INSERT INTO mfcc VALUES (%s) ' % col_var,x)
         conn.commit()   
-        print("The mfccs were sucessfully saved to the database")
     except Exception as e:
         print(e)
 
@@ -84,23 +77,32 @@ except Exception as e:
     print(e)
 
 
-  
+#collect directory names:
 dir_list = []
 for dirname in glob.glob('*/'):
     dir_list.append(dirname)
+if len(dir_list) > 0:
+    print("The directories found include: ", dir_list)
+else:
+    print("No directories found")
 
 
 try:
     for directory in dir_list:
         os.chdir(directory)
         dirname = directory[:-1]
-        print("Now processing wave files in the directory: "+dirname)
+        print("Now processing the directory: "+dirname)
         files_list = []
         for wav in glob.glob('*.wav'):
             files_list.append(wav)
         fl_df = pd.DataFrame(files_list)
-        print("Number of wave files to process: ",len(fl_df))
-        fl_df[0].apply(lambda x: get_save_mfcc(x,label,dirname,num_mfcc))
+        tot_wav = len(fl_df)
+        if tot_wav == 0:
+            print("No wave files found in ",dirname)
+        print("Number of wave files to process: ",tot_wav)
+        print("Progress:")
+        tqdm.pandas(ncols=1)
+        fl_df[0].progress_apply(lambda x: get_save_mfcc(x,label,dirname,num_mfcc,tot_wav))
         os.chdir("..")
         print("The Wave files in the "+ directory + " directory have been processed successfully")
 except Exception as e:
