0191a4697bb46bfddeb5c7ec7a0c6155b7e4022d
a-n-rose <37121771+a-n-rose@users.noreply.github.com>
2018-04-19 16:19:25 +0200

    Update extract_13MFCC_sqlite3.py
    Removed "apply(lambda..) " as that may cause memory issues for so much data. The previous script resulted in the computer to run out of memory. A for loop should solve that problem. Also implemented a progress display as well as a basic time tracker.

--- extract_13MFCC_sqlite3.py
+++ extract_13MFCC_sqlite3.py
@@ -3,22 +3,19 @@ This script is suited to exctract features from .wav files that are in one or mo
 
 There are 13 MFCCs, with 25ms window frames and 10ms shifts that are extracted.
 
-The MFCCs will be saved to a database in the cwd via SQLite3. The progress is printed by tqdm.
+The MFCCs will be saved to a database "sp_mfcc.db" in the cwd via SQLite3, in a table named "mfcc_13" 
+**if the num_mfcc is changed, be sure to change the name of the table from mfcc_13 to whatever name you'd like.
+**the number of columns of that table corresponds to 13 coefficients, +2 for the filename and label columns.
 
-note: to distinguish the differences of the files between the folders, the "label" 
-(i.e. the input provided by the user) is combined with the file's folder name and saved in the database.
+note: to distinguish the differences of the files between the folders, the "label" (i.e. the input provided by the user) 
+is combined with the file's folder name and saved in the database.
 '''
-
-
- 
-import os, sys, tarfile
-import numpy as np
+import os
 import pandas as pd
 import librosa
 import glob
 import sqlite3
-
-from tqdm import tqdm, tqdm_pandas
+import time
 
 
 def parser(file,num_mfcc):
@@ -26,11 +23,11 @@ def parser(file,num_mfcc):
     mfccs = librosa.feature.mfcc(y, sr, n_mfcc=num_mfcc,hop_length=int(0.010*sr),n_fft=int(0.025*sr))
     return mfccs
 
-def get_save_mfcc(tgz_file,label,dirname,num_mcff,tot_wav):
+def get_save_mfcc(tgz_file,label,dirname,num_mfcc):
     label = label+"_"+dirname
     try:
         filename = os.path.splitext(tgz_file)[0]
-        feature = parser(filename+".wav",num_mcff)
+        feature = parser(filename+".wav",num_mfcc)
         columns = list((range(0,num_mfcc)))
         column_str = []
         for i in columns:
@@ -49,7 +46,7 @@ def get_save_mfcc(tgz_file,label,dirname,num_mcff,tot_wav):
             else:
                 col_var+=' ?'
         print()
-        c.executemany(' INSERT INTO mfcc VALUES (%s) ' % col_var,x)
+        c.executemany(' INSERT INTO mfcc_13 VALUES (%s) ' % col_var,x)
         conn.commit()   
     except Exception as e:
         print(e)
@@ -62,7 +59,7 @@ c = conn.cursor()
 
 
 label = input("Which category is this speech? ")
-
+prog_start = time.time()
 #specify number of mfccs --> reflects the number of columns
 num_mfcc = 13
 columns = list((range(0,num_mfcc)))
@@ -72,7 +69,7 @@ for i in columns:
 
 try:    
     c.execute(''' CREATE TABLE IF NOT EXISTS mfcc_13(filename  text, %s, label text) ''' % ", ".join(column_type))
-    c.commit()
+    conn.commit()
 except Exception as e:
     print(e)
 
@@ -88,23 +85,21 @@ else:
 
 
 try:
-    for directory in dir_list:
+    for j in range(len(dir_list)):
+        directory = dir_list[j]
         os.chdir(directory)
         dirname = directory[:-1]
         print("Now processing the directory: "+dirname)
         files_list = []
         for wav in glob.glob('*.wav'):
             files_list.append(wav)
-        fl_df = pd.DataFrame(files_list)
-        tot_wav = len(fl_df)
-        if tot_wav == 0:
-            print("No wave files found in ",dirname)
-        print("Number of wave files to process: ",tot_wav)
-        print("Progress:")
-        tqdm.pandas(ncols=1)
-        fl_df[0].progress_apply(lambda x: get_save_mfcc(x,label,dirname,num_mfcc,tot_wav))
+        if len(files_list) == 0:
+            print("No wave files found in ", dirname)
+        for i in range(len(files_list)):
+            get_save_mfcc(files_list[i],label,dirname,num_mfcc)
+            print("Progress: ", (i/(len(files_list)-1))*100,"%  (",dirname,": ",j+1,"/",len(dir_list)," directories)")
         os.chdir("..")
-        print("The Wave files in the "+ directory + " directory have been processed successfully")
+        print("The Wave files in the "+ dirname + " directory have been processed successfully")
 except Exception as e:
     print(e)
 
@@ -113,5 +108,7 @@ try:
     conn.close()
     print("MFCC data has been successfully saved!")
     print("All wave files have been processed")
+    elapsed_time = time.time()-prog_start
+    print("Elapsed time in seconds: ", elapsed_time)
 except Exception as e:
     print(e)
