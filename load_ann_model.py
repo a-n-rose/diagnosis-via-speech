eeb69352f4af2da87db78d9a19e9b3065f2b1c1b
a-n-rose <37121771+a-n-rose@users.noreply.github.com>
2018-04-21 22:23:51 +0200

    Update load_ann_model.py
    update

--- load_ann_model.py
+++ load_ann_model.py
@@ -5,25 +5,92 @@ runs the model with "new" data and prints accuracy
 from keras.models import model_from_json
 import pandas as pd
 import numpy as np
+import sqlite3
+from sqlite3 import Error
+import time
 
 
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+        
+    return None
+
+def create_cursor(conn):
+    try:
+        return conn.cursor()
+    except Error as e:
+        print(e)
+
+def table2dataframe(c,table,lim = None):
+    try:
+        if lim:
+            limit = str(lim)
+            c.execute("SELECT * FROM "+table+" LIMIT " + limit)
+            data = c.fetchall()
+            df = pd.DataFrame(data)
+            return df
+        else:
+            c.execute("SELECT * FROM "+table)
+            data = c.fetchall()
+            df = pd.DataFrame(data)
+            return df
+    except Error as e:
+        print(e)
+    
+    return None
+
+start = time.time()
+
+print("connecting to database")
+conn = create_connection('sp_mfcc_new.db')
+c = create_cursor(conn)
 
 # load json and create model
-json_file = open('engerm_model_ann.json', 'r')
+file_weights = 'engerm_annweights_13mfcc_100epochs_1000000'
+file_model = 'engerm_annmodel_13mfcc_100epochs_1000000' 
+json_file = open(file_model+'.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-loaded_model.load_weights("engerm_weights_ann.h5")
+loaded_model.load_weights(file_weights+".h5")
 print("Loaded model from disk")
 
 
-#import "test" data (same data as from before... this is just to show it works)
-sp_df = pd.read_csv("engerm_mfcc_ml.csv",index_col = "Unnamed: 0")
-X = np.array(sp_df.ix[:,:-1])
-y = np.array(sp_df.ix[:,-1])
 
-# evaluate loaded model on test data
-loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-score = loaded_model.evaluate(X, y, verbose=0)
-print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+#import new data
+try:
+    #get new MFCC data
+    print("collecting new data --> df")
+    df_new = table2dataframe(c,'mfcc_13')
+
+
+except Exception as e:
+    print(e)
+
+df_new[14] = 0
+x1 = df_new.as_matrix()
+x = x1[:,2:]
+X = x[:,:-1]
+y = x[:,-1]
+
+# evaluate loaded model on new data
+try:
+    loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    score = loaded_model.evaluate(X, y, verbose=0)
+    print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+except Exception as e:
+    print(e)
+    
+    
+y_pred = loaded_model.predict(X)
+y_pred = (y_pred > 0.5)
+from sklearn.metrics import confusion_matrix
+y_test=y.astype(bool)
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(cm)
