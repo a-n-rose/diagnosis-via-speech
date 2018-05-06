2669fb4dac86646c09993db905a3a64ebb25a020
a-n-rose <37121771+a-n-rose@users.noreply.github.com>
2018-05-06 20:06:10 +0200

    Update check_tables_sqlite3_oop.py
    Added range and iqr

--- check_tables_sqlite3_oop.py
+++ check_tables_sqlite3_oop.py
@@ -1,8 +1,3 @@
-'''
-Currently changing scripts to use object oriented programming - work in progress
-
-'''
-
 import sqlite3
 from sqlite3 import Error
 import pandas as pd
@@ -13,22 +8,21 @@ import os
 import tarfile
 
 ##what I would like to be able to get from a class method
-#self.list_tables
+#self.list_tables(done)
 #self.depvar_type
 #self.depvar_numunique
-#self.columns
-#self.indvar_type
-#self.calc_std
-#self.calc_iqr
-#self.calc_range
+#self.columns (done)
+#self.indvar_type 
+#self.calc_std (done)
+#self.calc_iqr (done)
+#self.calc_range (done)
 
 class Explore_SQL:
     def __init__(self,db_name):
         self.database = db_name
-        self.depvar_type = "Not yet defined"
-        self.depvar_numunique = "Not yet defined"
         self.conn = sqlite3.connect(db_name)
         self.c = sqlite3.connect(db_name).cursor()
+        self.tables = "Not yet defined"
         
     def list_tables(self):
         try:
@@ -59,12 +53,22 @@ class Explore_Data:
         self.dataframe = df
         self.columns = df.columns.get_values()
         self.num_rows = len(df)
+        self.depvar_type = "Not yet defined"
+        self.depvar_numunique = "Not yet defined"
         
     def calc_std(self):
         data_std = [(col,np.std(self.dataframe[col])) for col in range(len(self.columns)) if isinstance(self.dataframe[col][0],float)]
         return(data_std)
-            
         
+    def calc_range(self):
+        data_range = [(col,(max(self.dataframe[col])-min(self.dataframe[col]))) for col in range(len(self.columns)) if isinstance(self.dataframe[col][0],float)]
+        return(data_range)
+    
+    def calc_iqr(self):
+        data_iqr = [(col,np.subtract(*np.percentile(self.dataframe[col],[75,25]))) for col in range(len(self.columns)) if isinstance(self.dataframe[col][0],float)]
+        return(data_iqr)
+                     
+                                
             
 curr_db = Explore_SQL('sp_mfcc.db')
 curr_db.database
@@ -77,16 +81,20 @@ curr_db.depvar_numunique
 tables = curr_db.list_tables()
 table1 = tables[0][1]
 table2 = tables[1][1]
-tab1_df = curr_db.table2dataframe(table1,lim=100)
-tab2_df = curr_db.table2dataframe(table2,lim=100)
-tab1_df
-tab2_df
+tab1 = curr_db.table2dataframe(table1,lim=100)
+tab2 = curr_db.table2dataframe(table2,lim=100)
+tab1
+tab2
 
 
 
-tab1_df = Explore_Data(tab1_df)
+tab1_df = Explore_Data(tab1)
 tab1_df.dataframe
 tab1_df.calc_std()
-tab2_df = Explore_Data(tab2_df)
+tab1_df.calc_range()
+tab1_df.calc_iqr()
+tab2_df = Explore_Data(tab2)
 tab2_df.dataframe
 tab2_df.calc_std()
+tab2_df.calc_range()
+tab2_df.calc_iqr()
