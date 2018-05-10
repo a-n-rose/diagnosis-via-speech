fe6e86cc2edfbe03359a2cf3bcbaee3e8b89f303
a-n-rose <37121771+a-n-rose@users.noreply.github.com>
2018-05-10 23:30:44 +0200

    Update checkSQL_main.py
    mode code a bit more elegant and added note on being able to look at categories of data

--- checkSQL_main.py
+++ checkSQL_main.py
@@ -6,6 +6,7 @@ Created on Thu May 10 12:29:05 2018
 @author: airos
 
 Does a quick check of SQL databases and tables.
+Can compare depending on categories (i.e. dependent variable)
 Input required.
 
 """
@@ -16,12 +17,33 @@ class Error_Msg:
     def __init__(self):
         self.msg = 'ERROR!'
         self.att = "\n"+('!*'*20)+"\n"
+        
+def str2index(int_string,items_list):
+    try:
+        ind = int(int_string)-1
+        if ind >= 0:
+            requested_item = items_list[ind]
+            return(requested_item,True)
+        else:
+            print("\nPlease enter an integer between 1 and "+str(len(items_list)))
+    except ValueError as ve:
+        print(Error_Msg().att)
+        print(ve)
+        print("\nValue must be an integer\n".upper())
+    except IndexError as ie:
+        print(Error_Msg().att)
+        print(ie)
+        if len(items_list) > 1:
+            err_msg = "\nPlease enter an integer between 1 and "+str(len(items_list))
+        else:
+            err_msg = "\nPlease enter '1' if you would like to continue"
+        print(err_msg.upper())
+        print(Error_Msg().att)
+    return None, False
+        
+    
     
 if __name__ == '__main__':
-    int_ValError = Error_Msg()
-    int_ValError.msg = "\nValue must be an integer\n"
-    index_Error = Error_Msg()
-    gen_Error = Error_Msg()
     
     dbs = Find_SQL_DB()
     dbs_list = dbs.db_list
@@ -33,26 +55,8 @@ if __name__ == '__main__':
         print("\nWhich database would you like to explore?")
         while database_entry == False:
             db_num = input("Please enter the number corresponding to the database: ")
-            try:
-                db_ind = int(db_num)-1
-                db_name = dbs_list[db_ind]
-                currdb = Explore_SQL(db_name)
-                database_entry = True
-            except ValueError as ve:
-                print(int_ValError.att)
-                print(ve)
-                print(int_ValError.msg.upper())
-            except IndexError as ie:
-                print(index_Error.att)
-                print(ie)
-                if len(dbs_list) > 1:
-                    err_msg = "\nPlease enter an integer between 1 and "+str(len(dbs_list))
-                    print(err_msg.upper())
-                else: 
-                    err_msg = "\nPlease enter '1' if you would like to continue"
-                    print(err_msg.upper())
-                print(index_Error.att)
-                
+            db_name, database_entry = str2index(db_num,dbs_list)
+        currdb = Explore_SQL(db_name)
         tables = currdb.tables2list()
         if tables:
             table_entry = False
@@ -62,28 +66,9 @@ if __name__ == '__main__':
             print("\nWhich table would you like to explore?")
             while table_entry == False:
                 table_num = input("Please enter the number corresponding to the table: ")
-                
-                try:
-                    table_ind = int(table_num)-1
-                    table_name = tables[table_ind]
-                    df = currdb.table2dataframe(table_name)
-                    currdf = Explore_Data(df)
-                    table_entry = True
-                except ValueError as ve:
-                    print(int_ValError.att)
-                    print(ve)
-                    print(int_ValError.msg.upper())
-                except IndexError as ie:
-                    print(index_Error.att)
-                    print(ie)
-                    if len(tables) > 1:
-                        err_msg = "\nPlease enter an integer between 1 and "+str(len(tables))
-                        print(err_msg.upper())
-                    else: 
-                        err_msg = "\nPlease enter '1' if you would like to continue"
-                        print(err_msg.upper())
-                    print(index_Error.att)
-    
+                table_name, table_entry = str2index(table_num,tables)
+            df = currdb.table2dataframe(table_name)
+            currdf = Explore_Data(df)
             currdf.print_profile(table_name)
             if currdf.depvar_numunique > 1:
                 cont_explore = False
@@ -96,39 +81,28 @@ if __name__ == '__main__':
                         cont_explore = True
                     else: 
                         print("\nPlease enter 'yes' or 'no'\n")           
-                while cont_explore == True:
+                dv_list = list(currdf.depvar)
+                pause_explore = False
+                while pause_explore == False:
                     print("\nDependent variables to explore: \n")
-                    dv_list = list(currdf.depvar)
                     for dv in range(len(dv_list)):
                         print("{}) ".format(dv+1),dv_list[dv])    
                     print("\nWhich dependent variable are you interested in?: ")
                     dv_num = input("Please enter the number corresponding to the variable: ")
-                    
-                    try:
-                        dv_ind = int(dv_num)-1
-                        dv_name = dv_list[dv_ind]
-                        currdf.print_profile(table_name,dv_name)
-                        again = False
-                        while again == False:
-                            expmore = input("\nWould you like to explore another dependent variable? (yes or no): ")
-                            if 'no' in expmore.lower():
-                                cont_explore = False
-                                break
-                            elif 'yes' in expmore.lower():
-                                again = True
-                            else:
-                                print("\nPlease enter 'yes' or 'no'\n")
-                                
-                    except ValueError as ve:
-                        print(int_ValError.att)
-                        print(ve)
-                        print(int_ValError.msg.upper())
-                    except IndexError as ie:
-                        print(index_Error.att)
-                        print(ie)
-                        err_msg = "\nPlease enter an integer between 1 and "+str(len(dv_list))
-                        print(err_msg.upper())
-                        print(index_Error.att)
+                    dv_name, pause_explore = str2index(dv_num,dv_list)
+                    if pause_explore == True:
+                        currdf.print_profile(table_name, dv_name)
+                    again = False
+                    while again == False and pause_explore == True:
+                        expmore = input("\nWould you like to explore another dependent variable? (yes or no): ")
+                        if 'no' in expmore.lower():
+                            pause_explore = True
+                            break
+                        elif 'yes' in expmore.lower():
+                            again = True
+                            pause_explore = False
+                        else:
+                            print("\nPlease enter 'yes' or 'no'\n")
                 
         else:
             print("\n!! No tables found in database\n")
