6d6db1eb1424d1fac5b0840bb5b93fed571d5bc0
Aislyn Rose <no email yet>
2018-05-17 17:11:03 +0200

    set up code to turn several .wav files into corresponding spectrograms in png

--- train_w_images/wav2image.py
+++ train_w_images/wav2image.py
@@ -9,25 +9,58 @@ Created on Tue Apr 24 20:29:10 2018
 import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.io import wavfile
+import glob
+import os
 
 
-sample_rate, samples = wavfile.read('H46/03_1SL/6VJ3NOS.WAV')
-frequencies, times, spectogram = signal.spectrogram(samples, sample_rate)
+
+###################################
+#for all files in a directory
+###################################
+os.chdir('./chunks/')
+wav_list = [db for db in glob.glob('*.wav')]
+samplerate_samples = [wavfile.read(filename) for filename in wav_list]
+freq_times_spec = [signal.spectrogram(samples,sample_rate) for sample_rate, samples in iter(samplerate_samples)]
+
+os.chdir('..')
+count = 0
+for freq, times, spectrogram in freq_times_spec:
+    if spectrogram.shape[0] == freq.shape[0] and spectrogram.shape[1] == times.shape[0]:        
+        fig=plt.figure()
+        ax=fig.add_subplot(1,4,1)
+        extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+        plt.pcolormesh(times,freq,spectrogram)
+        plt.imshow(spectrogram)
+        #plt.show()
+        count+=1
+        plt.savefig('images/wav2image_{}.png'.format(count),bbox_inches=extent, pad_inches=0,transparent=True, frameon=False,dpi = 500)
 
 
-fig=plt.figure()
-ax=fig.add_subplot(1,4,1)
-extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
 
-plt.pcolormesh(times, frequencies, spectogram)
-plt.axis('off')
-plt.imshow(spectogram)
+##################################
+#for a single file:
+##################################
+
+#filename = '2015-02-09-14-43-17_Kinect-Beam.wav'
+#sample_rate, samples = wavfile.read(filename)
+#frequencies, times, spectogram = signal.spectrogram(samples, sample_rate)
+#
+#
+#
+##fig=plt.figure()
+##ax=fig.add_subplot(1,4,1)
+##extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+#
+#plt.pcolormesh(times, frequencies, spectogram)
+#plt.imshow(spectogram)
+#plt.axis('off')
+#plt.show()
 
 
 
 #plt.ylabel('Frequency [Hz]')
 #plt.xlabel('Time [sec]')
 #plt.show()
-plt.savefig('spec2_sm3_high.png',bbox_inches=extent, pad_inches=0,transparent=True, frameon=False,dpi = 500)
+#plt.savefig('spec2_sm3_high.png',bbox_inches=extent, pad_inches=0,transparent=True, frameon=False,dpi = 100)
 
  
