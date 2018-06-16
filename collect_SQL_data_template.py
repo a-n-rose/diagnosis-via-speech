4f9ddc2d8f44d6264a268785e0eb1010e2a44bf9
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2018-06-16 22:43:28 +0200

    Wrote templates that are pretty easy to change depending on purpose. NOT DEBUGGED!!!! Just a start.

---
+++ collect_SQL_data_template.py
@@ -0,0 +1,121 @@
+import sqlite3
+from sqlite3 import Error
+import pandas as pd
+from pandas.api.types import is_string_dtype
+import numpy as np
+
+
+def does_db_exist(db_filename):
+    db_list = [db for db in glob.glob('*.db')]
+    if db_filename in db_list:
+        return True
+    return False
+
+
+class Extract_Data:
+    def __init__(self,db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.c = self.conn.cursor()
+    
+    def get_depvar_colname(self, table):
+        self.c.execute("PRAGMA table_info({})".format(table))
+        cols = self.c.fetchall()
+        #assumes dependent variable is the last column
+        depvarcolumn = cols[-1][1]
+        return(depvarcolumn)
+    
+    
+    def does_row_exist(self,table,row_num):
+        try:
+            self.c.execute("SELECT * FROM {} WHERE rowid = {}".format(table,row_num))
+            row = self.c.fetchall()
+            if row:
+                return True
+            else:
+                return False
+
+        except OperationalError as oe:
+            print(oe)
+            print("Check your input")
+           
+        return None
+    
+    
+    def does_var_exist(self,table,column,variable):
+        try:
+            self.c.execute("SELECT * FROM "+table+" WHERE "+column+" = '"+variable+"' LIMIT 1")
+            data = self.c.fetchall()
+            print(data)
+            if data:
+                return True
+        except Error as e:
+            print(e)
+        return None
+    
+    
+    def collect_data(self, table,column, variable,row_start,row_lim):
+        try:
+            if self.does_var_exist(table,column,variable):
+                row_start = str(row_start)
+                limit = str(row_lim)
+                max_row = str(row_start+row_lim)
+                if self.does_row_exist(table,max_row):
+                    self.c.execute("SELECT * FROM "+table+" WHERE label = '"+variable+"' LIMIT "+row_start+", "+limit)
+                    data = self.c.fetchall()
+                    df = pd.DataFrame(data)
+                    return(df)
+                else:
+                    print("Error: Not sufficient number of rows.")
+            else:
+                print("Error: Variable not found")
+        except Error as e:
+            print(e)
+            print("Data could not be collected")
+        
+        return None
+    
+    def reduce_df(self, df,num_cols):
+        #add 1 column if first column has strings (i.e. filenames, not mfcc data)
+        if is_string_dtype(df[df.columns[0]]):
+            num_cols += 1
+        cols_red = [i for i in range(num_cols)]
+        df_red = df[cols_red]
+        df_var = df[df.columns[-1]]
+        df_red = pd.concat([df_red,df_var],axis=1)
+        return df_red
+    
+    def prep_df(self,table,column,variable,row_start,row_lim,num_cols,int_classifer):
+        if self.does_var_exist(table,column,variable):
+            df = self.collect_data(table,column,variable,row_start,row_lim)
+            #Note: +2 for filename and dependent variable columns
+            if len(df.columns) > num_cols+2:
+                df = self.reduce_df(df,num_cols)
+            #turn dependent variable (i.e. last column) into integer classifier
+            df[df.columns[-1]] = int_classifer
+            return df
+        return None
+    
+    def get_startcol(self,matrix,include_1stMFCC):
+        startcol = 0
+        if isinstance(matrix[0][0],str):
+            startcol += 1
+        if include_1stMFCC == False:
+            startcol+= 1
+        return startcol
+        
+    
+    def get_dfcols(df):
+        cols = np.arange(df.shape[1])
+        return cols
+    
+    def close_conn_NOcommit(self):
+        if self.conn:
+            self.conn.close()
+        return None
+       
+    def close_conn_commit(self):
+        if self.conn:
+            self.conn.commit()
+            self.conn.close()
+        return None
+   
---
+++ train_keras_mfcc_template.py
@@ -0,0 +1,174 @@
+import os
+import logging.handlers
+import time
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix
+
+from collect_SQL_data_template import does_db_exist, Extract_Data
+from my_logger import start_logging, get_date
+
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    try:
+        start_time = time.time()
+        
+        ####################################################
+        #######################################################
+        #######################################################
+        
+        #MAKE SURE THESE VARIABLES ARE CORRECT!!!!!
+        #define variables:
+        script_purpose = 'trainmfcc' #will name logfile - generally don't need to change this unless training something other than MFCCs
+        current_filename = os.path.basename(__file__)
+        session_name = get_date() #make sure this session has a unique identifier - link to model name and logging information
+        table = 'table' 
+        variable_train = 'variable_train' 
+        variable_test = 'variable_test'
+        gen_notes = 'General notes about the current session' #whatever you'd like to note down/document
+        
+        rowstart_train = 0  #the row to start pulling data from database
+        rowlim_train = 1000000  #limit the number of rows
+        rowstart_test = 0
+        rowlim_test = 1000000
+        num_mfcc = 13  #up to 40 (depending on the data)
+        include_1stMFCC = False  #True includes coefficient, which is correlated with loudness of speech
+        language = 'language'
+        int_classifier = None #0=English, 1=German  #which integer should be used to classify the langauge? Note: if more than 2 (i.e. 0,1,2) need to change classifier settings
+        
+        #for initializing (keras) model/classifier:
+        classifier = 'Sequential()'
+        activation_layer = 'relu'
+        kernel_initializer = 'uniform'
+        activation_output = 'sigmoid'
+        optimizer = 'adam'
+        loss = 'binary_crossentropy'
+        metrics = ['accuracy']
+        
+        #for training the model:
+        batch_size = 10
+        epochs = 100
+        num_rows = 2000000
+        
+        #names to save modoel:
+        model_name = 'dl_{}_{}_{}'.format(session_name,script_purpose,num_rows)
+        
+        #######################################################
+        #######################################################
+        #######################################################
+        #######################################################
+        
+        start_logging(script_purpose)
+        logging.info("Running script: {}".format(current_filename))
+        logging.info("Session: {}".format(session_name))
+        logging.info("Language: {}".format(language))
+        logging.info("Integer label: {}".format(int_classifier))
+        logging.info("Table: {}".format(table))
+        logging.info("Variable for training: {}".format(variable_train))
+        logging.info("Variable for testing: {}".format(variable_test))
+        logging.info("Row start (train): {}\nRow limit (train): {}".format(rowstart_train,rowlim_train))
+        logging.info("Row start (test): {}\nRow limit (test): {}".format(rowstart_test,rowlim_test))
+        logging.info("MFCC number: {}".format(num_mfcc))
+        logging.info("1st MFCC included? {}".format(include_1stMFCC))
+        logging.info("Notes: {}".format(gen_notes))
+        
+        
+        ########### Starting Program ############
+        
+        db_name = 'db_name'
+        if does_db_exist(db_name):
+            currdb = Extract_Data(db_name)
+            column = currdb.get_depvar_colname(table)
+            
+            #prep data for training
+            df_train = currdb.prep_df(table,column,variable_train,rowstart_train,rowlim_train,num_mfcc,int_classifier)
+    
+            x1_train = df_train.as_matrix()
+            startcol_train = currdb.get_startcol(x1_train,include_1stMFCC)
+            x_train = x1_train[:,startcol_train:]
+            
+            X_train = x_train[:,:-1]
+            y_train = x_train[:,-1]
+            
+            #prep train data 
+            df_test = currdb.prep_df(table,column,variable_test,rowstart_test,rowlim_test,num_mfcc,int_classifier)
+            
+            x1_test = df_test.as_matrix()
+            startcol_test = currdb.get_startcol(x1_test,include_1stMFCC)
+            x_test = x1_test[:,startcol_test:]
+            
+            X_test = x_test[:,:-1]
+            y_test = x_test[:,-1]
+            
+            num_labels = len(np.unique(y_train))
+            if num_labels == 2:
+                num_labels = 1
+            
+            filter_size = 2
+            num_inputs = X_train.shape[1]
+            
+            ###### Building Classifier ######
+            
+            classifier = Sequential()
+            num_outputs = num_labels
+            #used the average between number of input features and output labels
+            av_inout = int((num_inputs+num_outputs)/2)
+            
+            #add input layer and first hidden layer:
+            classifier.add(Dense(activation, units=av_inout, input_dim=num_inputs, kernel_initializer))
+            
+            #add second hidden layer:
+            classifier.add(Dense(activation, units = av_inout, kernel_initializer))
+            
+            #add the output layer:
+            classifier.add(Dense(activation, units = num_outputs, kernel_initializer))
+            
+            #compile ANN
+            #'binary_crossentropy' for binary output label
+            classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy',metrics = ['accuracy'])
+            
+            
+            #Document in loggin system:
+            logging.info("classifier = {}\nnum_outputs = {}\nav_inout = int(({}+{})/2)\nclassifier.add(Dense(activation={}, units=av_inout, input_dim=num_inputs, kernel_initializer={}))\nclassifier.add(Dense(activation = {}, units = av_inout, kernel_initializer = {}))\nclassifier.add(Dense(activation = {}, units = num_outputs, kernel_initializer = {}))\nclassifier.compile(optimizer = {}, loss = {},metrics = {})".format(classifier,num_labels,num_inputs,num_outputs, activation_layer, kernel_initializer, activation_layer, kernel_initializer, activation_output, kernel_initializer, optimizer, loss, metrics))
+            
+            
+            #training model
+            classifier.fit(X_train,y_train,batch_size,epochs)
+            
+            y_pred = classifier.predict(X_test)
+            y_pred = (y_pred > 0.5)
+            
+            y_test=y_test.astype(bool)
+            cm = confusion_matrix(y_test, y_pred)
+            logging.info("Confusion Matrix for {}:\n{}".format(language,cm))
+            acc = "%s: %.2f%%" % (classifier.metrics_names[1], score[1]*100)
+            logging.info(acc)
+            print("Model Evaluation:")
+            print(acc)
+            
+            logging.info("Saving model and weights as: \n{}.json\n{}.h5".format(model_name,model_name))
+            
+            model_json = classifier.to_json()
+            with open(model_name+'.json',w) as json_file:
+                json_file.write(model_json)
+            classifier.save_weights(model_name+'.h5')
+            
+            
+            end_time = time.time()
+            
+            total_duration = (end_time - start_time)/3600
+            logging.info("Total time to complete task: {} hours".format(total_duration))
+            print("Finished training model - model and weights are saved.")
+
+        
+    except Exception as e:
+        logging.info("Error occurred: %s" % e)
+        
+    finally:
+        if currdb.conn:
+            currdb.conn.close()
+            logging.info("Database successfully closed")
